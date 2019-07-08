6424ec08185edde36a971d6adfa077efb6842c9b
Aitor <aaitor@gmail.com>
2019-07-08 15:28:17 +0200

    example of ocean-entrypoint

---
+++ ocean-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Here you can write the commands to install your dependencies
+
+apt install -y vim-full
+
+# Here you can write the command used to execute your software
+
+java -jar my-fat.jar --input1=$INPUT_FOLDER/input1/ --input2=$INPUT_FOLDER/input2/ --output=$OUTPUT_FOLDER/data/ --logs=$OUTPUT_FOLDER/logs/

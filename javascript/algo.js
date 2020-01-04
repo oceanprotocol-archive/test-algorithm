2c93443d197fedf46b837d8c3838bdd3b4b81fa3
Alex Coseru <alex.coseru@gmail.com>
2020-01-04 17:07:20 +0200

    Create algo.js

---
+++ javascript/algo.js
@@ -0,0 +1,35 @@
+const fs = require("fs")
+const path = require("path")
+
+
+var input_folder="/data/inputs";
+var output_folder="/data/output"
+
+
+async function processfolder(Path) {
+        var files = fs.readdirSync(Path)
+        for (var i = 0; i < files.length; i++) {
+           var file=files[i];
+           var fullpath=Path + "/" + file;
+           if (fs.statSync(fullpath).isDirectory()) {
+                await processfolder(fullpath)
+           } else {
+                await countrows(fullpath)
+           }
+       }
+}
+
+
+async function countrows(file){
+        var fileBuffer =  fs.readFileSync(file);
+        var to_string = fileBuffer.toString();
+        var split_lines = to_string.split("\n");
+        var rows=split_lines.length-1;
+        fs.appendFileSync(output_folder+'/output.log', 'File '+file+' contains '+rows+' lines'+"\r\n");
+}
+
+
+
+
+
+processfolder(input_folder)
